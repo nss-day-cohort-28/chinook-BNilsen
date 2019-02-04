cbb5fdde7982fb4f4504e5ad0de1c6e3eeacc4bb
BryanNilsen <bryannilsen@gmail.com>
2019-02-04 12:07:21 -0600

    completed queries 7 through 13

--- chinook.sql
+++ chinook.sql
@@ -1,13 +1,8 @@
 -- Instructions
--- In your cohort's Github account, create a github repository for your answers. If your instructor is using Github Classroom, the repository will be created for you. If not, name your repository chinook-{your Github username].
--- If you are using the Firefox Add On, and you have trouble opening it once it has been installed, try opening Firefox, selecting "customize" at the bottom of the hamberger menu at the top right of the page. You can then drag SQLite Manager onto the toolbar where it will be easily accessible.
 
--- Go ahead and click around a little bit to familarize yourself with the database
--- Recommended: Draw an ERD of the Chinook database. Make sure to label primary keys, foreign keys, and indicate the type of relationship (one to one, one to many, many to many, etc) for each relationship. Use draw.io.
--- When you're ready to start the exercise, click the tab labeled "Execute SQL", type in your query and run it.
--- If your query is correct - meaning it returns the data requested - copy and the SQL statement and paste it into the corresponding file for the exercise (see below). If your query doesn't return the expected results, try try again.
+-- Recommended: Draw an ERD of the Chinook database. Make sure to label primary keys, foreign keys, and indicate the type of relationship (one to one, one to many, many to many, etc) for each relationship. (used lucidchart)
+
 -- For each of the following exercises, provide the appropriate query. Yes, even the ones that are expressed in the form of questions. Everything from class and the Sqlite Documentation is fair game.
-
 
 
 -- #1: Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
@@ -44,25 +39,55 @@ FROM Invoice i
 JOIN Customer c ON i.CustomerId=c.CustomerId
 JOIN Employee e ON c.SupportRepId = e.EmployeeId
 
--- invoice_totals.sql: Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers.
+-- #7: Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers.
+-- (ordered by Customer Last Name)
+SELECT i.Total, c.FirstName AS CustomerName, c.LastName AS CustomerLastName, c.Country, e.FirstName AS AgentFirstName, e.LastName AS AgentLastName
+FROM Invoice i
+JOIN Customer c ON i.CustomerId=c.CustomerId
+JOIN Employee e ON c.SupportRepId = e.EmployeeId
+ORDER BY CustomerLastName
 
--- total_invoices_{year}.sql: How many Invoices were there in 2009 and 2011?
+-- #8: How many Invoices were there in 2009 and 2011?
+SELECT COUNT(i.InvoiceId)
+FROM Invoice i
+WHERE (i.InvoiceDate LIKE '2009%'
+OR i.invoiceDate LIKE  '2011%')
 
--- total_sales_{year}.sql: What are the respective total sales for each of those years?
+-- #9: What are the respective total sales for each of those years?
+-- (named columns 2009 and 2011 respectively)
+SELECT SUM(CASE WHEN i.InvoiceDate LIKE '2009%' THEN i.Total END) as '2009',
+       SUM(CASE WHEN i.InvoiceDate LIKE '2011%' THEN i.Total END) as '2011'
+FROM Invoice i
 
--- invoice_37_line_item_count.sql: Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37.
+-- #10: Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37.
+SELECT COUNT()
+FROM InvoiceLine i
+WHERE i.InvoiceId=37
 
--- line_items_per_invoice.sql: Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY
+-- #11: Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY
+SELECT COUNT() AS LineCount
+FROM InvoiceLine l
+JOIN Invoice i
+WHERE l.InvoiceId=i.InvoiceId
+GROUP BY i.InvoiceId
 
--- line_item_track.sql: Provide a query that includes the purchased track name with each invoice line item.
+-- #12: Provide a query that includes the purchased track name with each invoice line item.
+SELECT t.Name
+FROM InvoiceLine i
+JOIN Track t on i.TrackId=t.TrackId
 
--- line_item_track_artist.sql: Provide a query that includes the purchased track name AND artist name with each invoice line item.
+-- #13: Provide a query that includes the purchased track name AND artist name with each invoice line item.
+SELECT t.Name, r.Name
+FROM InvoiceLine i
+JOIN Track t on i.TrackId=t.TrackId
+JOIN Album a on t.AlbumId=a.AlbumId
+JOIN Artist r on a.ArtistId=r.ArtistId
 
--- country_invoices.sql: Provide a query that shows the # of invoices per country. HINT: GROUP BY
+-- #14: Provide a query that shows the # of invoices per country. HINT: GROUP BY
 
--- playlists_track_count.sql: Provide a query that shows the total number of tracks in each playlist. The Playlist name should be include on the resulant table.
+-- #15: Provide a query that shows the total number of tracks in each playlist. The Playlist name should be include on the resulant table.
 
--- tracks_no_id.sql: Provide a query that shows all the Tracks, but displays no IDs. The result should include the Album name, Media type and Genre.
+-- #16: Provide a query that shows all the Tracks, but displays no IDs. The result should include the Album name, Media type and Genre.
 
 -- invoices_line_item_count.sql: Provide a query that shows all Invoices but includes the # of invoice line items.
 
